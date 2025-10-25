7bc969afee0f508881feb850459c0f565a874c8f
Bingbing Ma <bingbingma2003@gmail.com>
2025-10-25 00:34:09 -0700

    Initial Commit

---
+++ TravelVerse/TravelVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelVerseApp.swift
+//  TravelVerse
+//
+//  Created by Bingbing Ma on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
